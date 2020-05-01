3d6dbc5505a7278a08a5fb46a0fc455d6fcc92fd
hwanghyojun <hhjsk12@gmail.com>
2020-05-01 12:16:36 +0900

    CircularProgress

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -17,6 +18,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress:{
+    margin: theme.spacing(3)
   }
 });
 
@@ -24,11 +28,13 @@ const styles = theme => ({
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   //api서버에  접근해서 데이터를 받아오는 작업은 여기서
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -38,6 +44,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
 
@@ -69,7 +80,12 @@ class App extends Component {
               job={c.job}
               />
             )
-          }) : ""}</TableBody>
+          }) :
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+              </TableCell>
+              </TableRow>}</TableBody>
         </Table>
       </Paper>
     );
